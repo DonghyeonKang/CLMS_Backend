61327af87a6259df8e7a94d5cff7aa00ba5fee31
DonghyeonKang <86303312+DonghyeonKang@users.noreply.github.com>
2024-03-13 15:35:13 +0900

    Feat: 인증인가 필터 적용

--- src/main/java/com/example/clms/config/SecurityConfig.java
+++ src/main/java/com/example/clms/config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.example.clms.config;
 
+import com.example.clms.config.jwt.JwtAuthenticationFilter;
+import com.example.clms.config.jwt.JwtAuthorizationFilter;
+import com.example.clms.config.jwt.JwtService;
+import com.example.clms.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +20,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -23,6 +32,8 @@ public class SecurityConfig {
                         .requestMatchers( "/swagger-ui/**", "/v3/api-docs/**", "/register/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtService))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository, jwtService))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
 
@@ -32,5 +43,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
